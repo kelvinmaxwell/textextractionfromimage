e38b3881b861e12bfb444a22b484ed8d6e81a701
user <kelvinmaxwell82@gmail.com>
2022-07-29 02:25:00 +0300

    Initial commit

---
+++ app/src/main/java/com/karimax/textextractionfromimage/gettext.java
@@ -0,0 +1,20 @@
+package com.karimax.textextractionfromimage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+
+public class gettext extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gettext);
+
+
+
+    }
+}
